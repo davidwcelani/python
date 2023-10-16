10b05c8294060d71223157c1eb86e474c032e1b4
davidwcelani <williancelani@hotmail.com>
2023-10-16 13:09:32 -0300

    tratamento de imagem

--- teste.py
+++ teste.py
@@ -3,9 +3,15 @@ import numpy as np
 
 imagem = cv2.imread("img.jpg")
 
-#imagem[170:180, 70:80] = (255, 255, 255)
-print(imagem[175][75])
+imagem_normalizada = imagem / 255.0
 
+alpha = 300
 
-cv2.imshow("Imagem", imagem)
+imagem_com_contraste = cv2.convertScaleAbs(imagem_normalizada, alpha=alpha, beta=0)
+
+imagem_reducao_ruido = cv2.medianBlur(imagem_com_contraste, 3)
+
+cv2.imshow("Imagem Original", imagem)
+cv2.imshow("Imagem Tratada", imagem_com_contraste)
+cv2.imshow("Imagem Sem Ruído", imagem_reducao_ruido)
 cv2.waitKey(0)
